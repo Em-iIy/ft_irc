c424e54024c429c432f6d6e439ffd84ad320d558
Emily Winnink <emily.winnink@gmail.com>
2023-11-28 15:19:44 +0100

    fixed PING to be more accurate

--- src/cmds/PING.cpp
+++ src/cmds/PING.cpp
@@ -5,22 +5,31 @@ void	Message::_PING(void)
 	// Must be registered to use this command
 	if (this->_user.getRegistered() == false)
 		return ;
+	if (this->_param == "")
+	{
+		// 409		ERR_NOORIGIN
+		this->_response = ":" + this->_server.getServerName() + " 409 :No origin specified\r\n";
+		this->_respondUser();
+		return ;
+	}
 		
+	std::string	src;
 	std::string	dst;
-	std::string	msg;
-	size_t		colon;
+	std::string rplMsg;
 	size_t		space;
 
 	space = this->_param.find(' ');
-	colon = this->_param.find(':');
-	if (space != std::string::npos && colon != std::string::npos)
+	if (space != std::string::npos)
 	{
-		dst = this->_param.substr(0, space);
-		msg = this->_param.substr(colon + 1);
+		src = this->_param.substr(0, space);
+		dst = this->_param.substr(space + 1);
+		rplMsg = dst + " :" + src;
 	}
 	else
 	{
+		src = "";
 		dst = this->_param;
+		rplMsg = dst;
 	}
 	if (dst != this->_server.getServerName())
 	{
@@ -29,7 +38,7 @@ void	Message::_PING(void)
 		this->_respondUser();
 		return ;
 	}
-	this->_response = ":" + this->_server.getServerName() + " PONG " + this->_server.getServerName() + " :" + msg + "\r\n";
+	this->_response = ":" + this->_server.getServerName() + " PONG " + rplMsg + "\r\n";
 	this->_respondUser();
 	return ;
 }
